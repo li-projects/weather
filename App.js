d1c48571136b07a0315715e421da592916170ee5
Li Lee <leesquare@gmail.com>
2021-05-25 00:06:28 -0600

    initial commit

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, TextInput, KeyboardAvoidingView } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="height">
+      <Text style={[styles.largeText, styles.textStyle]}>
+        San Francisco
+      </Text>
+      <Text style={[styles.smallText, styles.textStyle]}>
+        Light Cloud
+      </Text>
+      <Text style={[styles.largeText, styles.textStyle]}>
+        24°
+      </Text>
+      <TextInput
+      autoCorrect={false}
+      placeholder="Search any city"
+      placeholderTextColor="white"
+      style={styles.textInput}
+      clearButtonMode="always"
+      />
+      </KeyboardAvoidingView>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +34,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    textAlign: 'center',
+    ...Platform.select({
+      ios: {
+        fontFamily: 'AvenirNext-Regular'
+      },
+      android: {
+        fontFamily: 'Roboto'
+      }
+    })
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18
+  },
+  textInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center',
+  }
 });
